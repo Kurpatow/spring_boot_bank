26b1f2bc3de13ae63ea5eb85d82e2dffe46125ae
Kurpatow <113792694+Kurpatow@users.noreply.github.com>
2023-05-18 17:16:13 +0300

    Initial commit

---
+++ src/main/java/com/example/yokodspringbootbank/YokoDSpringBootBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.yokodspringbootbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YokoDSpringBootBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YokoDSpringBootBankApplication.class, args);
+	}
+
+}
