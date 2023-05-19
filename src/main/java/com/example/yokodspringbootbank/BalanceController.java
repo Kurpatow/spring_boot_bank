378b654f154815c8159f5c345c296a86d7c74763
Kurpatow <113792694+Kurpatow@users.noreply.github.com>
2023-05-19 23:26:26 +0300

    start

--- src/main/java/com/example/yokodspringbootbank/BalanceController.java
+++ src/main/java/com/example/yokodspringbootbank/BalanceController.java
@@ -1,26 +1,29 @@
 package com.example.yokodspringbootbank;
 
 import com.example.yokodspringbootbank.model.TransferBalance;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
 @RestController("/balance")
+@AllArgsConstructor
 public class BalanceController {
+
+    private final BankService bankService;
 
     @GetMapping("/{accountId}")
     public BigDecimal getBalance(@PathVariable Long accountId) {
-
+        return bankService.getBalance(accountId);
     }
 
     @PostMapping("/add")
-    public BigDecimal addMoney(@RequestBody TransferBalance amount) {
-
+    public BigDecimal addMoney(@RequestBody TransferBalance transferBalance) {
+        return bankService.addMoney(transferBalance.getTo(),transferBalance.getAmount());
     }
 
     @PostMapping("/transfer")
-    public BigDecimal transfer(@RequestBody TransferBalance transferBalance) {
-
+    public void transfer(@RequestBody TransferBalance transferBalance) {
+        bankService.makeTransfer(transferBalance);
     }
-
 }
--- src/main/java/com/example/yokodspringbootbank/SpringBootBankApplication.java
+++ src/main/java/com/example/yokodspringbootbank/SpringBootBankApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YokoDSpringBootBankApplication {
+public class SpringBootBankApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YokoDSpringBootBankApplication.class, args);
+		SpringApplication.run(SpringBootBankApplication.class, args);
 	}
 
 }
