e11e4c0de4aa1cb84e9b0e637064692e35f2f158
Kurpatow <113792694+Kurpatow@users.noreply.github.com>
2023-06-07 13:42:40 +0300

    start

--- src/main/java/com/example/yokodspringbootbank/BalanceController.java
+++ src/main/java/com/example/yokodspringbootbank/BalanceController.java
@@ -2,10 +2,12 @@ package com.example.yokodspringbootbank;
 
 import com.example.yokodspringbootbank.model.TransferBalance;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 
+@Slf4j
 @RestController("/balance")
 @AllArgsConstructor
 public class BalanceController {
@@ -25,5 +27,11 @@ public class BalanceController {
     @PostMapping("/transfer")
     public void transfer(@RequestBody TransferBalance transferBalance) {
         bankService.makeTransfer(transferBalance);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String handle(IllegalArgumentException e){
+        log.error(e.getMessage());
+        return "I broke down";
     }
 }
